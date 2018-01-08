0edfe2995ff5268b36715049da3fbb585eceaf3f
Leon Akasaka <leon@wisdomsoft.jp>
2018-01-09 03:16:11 +0900

    Add ResourceProviderExtensions.

---
+++ Levolution.Resources/ResourceProviderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace Levolution.Resources
+{
+    public static class ResourceProviderExtensions
+    {
+        public static async Task<T> LoadOrDefault<TResourceIdentifier, T>(this IResourceProvider<TResourceIdentifier> resourceProvider, TResourceIdentifier id)
+        {
+            var r = await resourceProvider.LoadAsync<T>(id);
+            return r.LoadingState == LoadingState.Success ? r.Value : default(T);
+        }
+    }
+}
