5b4663b0a6801ca79a3a25bc9735b755130667db
Leon Akasaka <leon@wisdomsoft.jp>
2018-01-13 02:18:19 +0900

    Add UnitTest.

---
+++ Levolution.Resources.UnitTest/MemoryResourceProviderTest.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+
+namespace Levolution.Resources.UnitTest
+{
+    [TestClass]
+    public class MemoryResourceProviderTest
+    {
+        [TestMethod]
+        public async Task TestLoadAsync()
+        {
+            var rp = new MemoryResourceProvider<int>();
+
+            var value1 = "Stand by Ready!";
+            var value2 = new Uri("https://github.com/LeonAkasaka/Levolution.Resources");
+
+            rp.Write(1, value1);
+            rp.Write(2, value2);
+
+            var r1 = await rp.LoadAsync<string>(1);
+            Assert.AreEqual(LoadingState.Success, r1.LoadingState);
+            Assert.AreEqual(value1, r1.Value);
+
+            var r2 = await rp.LoadAsync<Uri>(2);
+            Assert.AreEqual(LoadingState.Success, r2.LoadingState);
+            Assert.AreEqual(value2, r2.Value);
+
+            var r3 = await rp.LoadAsync<object>(1); // Base type
+            Assert.AreEqual(LoadingState.Success, r3.LoadingState);
+            Assert.AreEqual(value1, r3.Value);
+
+            var r4 = await rp.LoadAsync<object>(2); // base type
+            Assert.AreEqual(LoadingState.Success, r4.LoadingState);
+            Assert.AreEqual(value2, r4.Value);
+
+            var r5 = await rp.LoadAsync<Uri>(1); // Type mismatch
+            Assert.AreEqual(LoadingState.Failure, r5.LoadingState);
+
+            var r6 = await rp.LoadAsync<string>(2); // Type mismatch
+            Assert.AreEqual(LoadingState.Failure, r6.LoadingState);
+
+            var r7 = await rp.LoadAsync<object>(0); // Id not found.
+            Assert.AreEqual(LoadingState.NotFound, r7.LoadingState);
+        }
+    }
+}
---
+++ Levolution.Resources.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Levolution.Resources.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Levolution.Resources.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ab9f582b-285b-473a-9504-133bf88f2673")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
