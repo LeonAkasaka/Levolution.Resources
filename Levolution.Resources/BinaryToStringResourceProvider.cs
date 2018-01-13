1ee95529369c8f914d669c95de11b5490129a683
Leon Akasaka <leon@wisdomsoft.jp>
2018-01-14 02:43:52 +0900

    Add ResourceProviders

---
+++ Levolution.Resources/BinaryToStringResourceProvider.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+
+namespace Levolution.Resources
+{
+    public class BinaryToStringResourceProvider<TResourceIdentifier> : WrappedResourceProvider<TResourceIdentifier>
+    {
+        public Encoding Encoding { get; }
+
+        public BinaryToStringResourceProvider(IResourceProvider<TResourceIdentifier> provider) : this(provider, Encoding.UTF8) { }
+
+        public BinaryToStringResourceProvider(IResourceProvider<TResourceIdentifier> provider, Encoding encoding) : base(provider)
+        {
+            Encoding = encoding;
+        }
+
+        protected async override Task<ResourceResult<T>> LoadFromInnerResourceProvider<T>(TResourceIdentifier id)
+        {
+            if (typeof(T).IsAssignableFrom(typeof(string))) { return ResourceResult<T>.NotFound; }
+
+            var result = await base.LoadFromInnerResourceProvider<byte[]>(id);
+            if (result.ResourceState == ResourceState.NotFound) { return ResourceResult<T>.NotFound; }
+            else if (result.ResourceState == ResourceState.Failure) { return ResourceResult<T>.Failure; }
+            else if (result.ResourceState == ResourceState.None) { return default; }
+            else { return new ResourceResult<T>((T)(object)Encoding.GetString(result.Value)); }
+        }
+    }
+}
---
+++ Levolution.Resources/FileResourceProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Levolution.Resources
+{
+    public class FileResourceProvider : IResourceProvider<string>
+    {
+        public string RootPath { get; }
+        
+        public FileResourceProvider(string rootPath) => RootPath = rootPath;
+
+        public async Task<ResourceResult<T>> LoadAsync<T>(string id)
+        {
+            try
+            {
+                var path = Path.Combine(RootPath, id);
+                if (typeof(IEnumerable<byte>).IsAssignableFrom(typeof(T)))
+                {
+                    var data = await Task.Run(() => File.ReadAllBytes(path));
+                    return new ResourceResult<T>((T)(object)data);
+                }
+                return ResourceResult<T>.Failure;
+            }
+            catch(FileNotFoundException e)
+            {
+                return ResourceResult<T>.NotFound;
+            }
+            catch
+            {
+                return ResourceResult<T>.Failure;
+            }
+        }
+    }
+}
