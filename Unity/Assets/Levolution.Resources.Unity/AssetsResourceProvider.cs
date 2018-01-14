ec4a88f66e285469e5a3253acc7fdaf39ddc329c
Leon Akasaka <leon@wisdomsoft.jp>
2018-01-15 01:00:55 +0900

    Add ResourceProvider implimentation for Unity

---
+++ Unity/Assets/Levolution.Resources.Unity/AssetsResourceProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Levolution.Resources.Unity
+{
+    public class AssetsResourceProvider : IResourceProvider<string>
+    {
+        public string RootPath { get; set; }
+
+        public AssetsResourceProvider() : this("") { }
+        public AssetsResourceProvider(string rootPath) { RootPath = rootPath; }
+
+        public Task<ResourceResult<T>> LoadAsync<T>(string id)
+        {
+            if (!typeof(UnityEngine.Object).IsAssignableFrom(typeof(T)))
+            {
+                return Task.FromResult(ResourceResult<T>.NotFound);
+            }
+
+            var tcs = new TaskCompletionSource<ResourceResult<T>>();
+            var path = Path.Combine(RootPath, id);
+            var r = UnityEngine.Resources.LoadAsync(path, typeof(T));
+
+            Action<UnityEngine.AsyncOperation> OnComplited = null;
+            OnComplited = _ =>
+            {
+                tcs.SetResult(r.asset == null ? ResourceResult<T>.NotFound : new ResourceResult<T>((T)(object)r.asset));
+                r.completed -= OnComplited;
+            };
+
+            r.completed += OnComplited;
+            return tcs.Task;
+        }
+    }
+}
---
+++ Unity/Assets/Sample.cs
@@ -0,0 +1,23 @@
+﻿using Levolution.Resources;
+using Levolution.Resources.Unity;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Sample : MonoBehaviour {
+
+    private IResourceProvider<string> _resourceProvider;
+
+    private void Start()
+    {
+        _resourceProvider = new AssetsResourceProvider();
+        CreateCube();
+    }
+
+    async Task CreateCube()
+    {
+        var prefab = await _resourceProvider.LoadOrDefault<string, GameObject>("Cube");
+        var cube = Instantiate(prefab);
+        cube.transform.parent = transform;
+    }
+}
